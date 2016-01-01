c98506d9607a0102b620c6374b3fc1632ef4c319
Kishan Mundha <kishan.mundha@gmail.com>
2016-01-02 01:01:27 +0530

    formatting

--- src/app/controllers/projectListCtrl.js
+++ src/app/controllers/projectListCtrl.js
@@ -1,13 +1,12 @@
-﻿
 (function () {
     'use strict';
 
     var app = angular.module('app');
 
     app.controller('projectListCtrl', ['$scope', 'docService', function ($scope, docService) {
-        $scope.projects = docService.getProjectList();
+            $scope.projects = docService.getProjectList();
 
-        docService.setCurrentProject(false);
-    }]);
+            docService.setCurrentProject(false);
+        }]);
 
 })();
