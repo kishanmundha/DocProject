8b5bb8243d522d29bb8f2f03641bd03012b79a71
Kishan Mundha <kishan.mundha@gmail.com>
2016-01-02 00:58:00 +0530

    changes to editing feature

--- src/app/controllers/editDocCtrl.js
+++ src/app/controllers/editDocCtrl.js
@@ -1,65 +1,163 @@
-﻿
-        (function () {
-            'use strict';
+(function () {
+    'use strict';
 
-            var app = angular.module('app');
+    var app = angular.module('app');
 
-            app.controller('editDocCtrl', ['$scope', '$routeParams', '$sce', '$window', 'docService', function ($scope, $routeParams, $sce, $window, docService) {
-                    var projectId = $routeParams.projectId;
-                    var docId = $routeParams.docId;
+    app.controller('editDocCtrl', ['$log', '$scope', '$routeParams', '$sce', '$window', 'docService', '$timeout', 'localStorageService', 'config',
+        function ($log, $scope, $routeParams, $sce, $window, docService, $timeout, localStorageService, config) {
+            $log.debug('editDocCtrl called');
 
-                    $scope.projectId = projectId;
-                    $scope.docId = docId;
+            var projectId = $routeParams.projectId;
+            var docId = $routeParams.docId;
 
-                    $scope.docContent;
+            var autoSaveDuration = config.editDoc.autoSaveDuration || (1000 * 60);
+            var autoSaveExpiry = config.editDoc.autoSaveExpiry || (1000 * 60 * 60);    // 1 hour
 
-                    $scope.markdown = {};
+            $scope.projectId = projectId;
+            $scope.docId = docId;
+            
+            var timer;
 
-                    //console.debug($scope);
+            $scope.config = config;
 
-                    docService.getDocContent(projectId, docId, function (data) {
+            $scope.docContent;
 
-                        if (Object.prototype.toString.call(data) === '[object Object]') {
-                            data = data.data;
-                        }
+            $scope.markdown = {};
 
-                        $scope.markdown.inputText = data;
+            //console.debug($scope);
 
-                        //parseMarkdownContent();
-                    });
+            var saveToCache = function () {
+                var obj = {
+                    timestamp: (new Date()).getTime(),
+                    content: $scope.markdown.inputText
+                };
+                
+                var key = projectId;
+                
+                if(docId)
+                    key += '/' + docId;
 
-                    var trustAsHtml = function (string) {
-                        return $sce.trustAsHtml(string);
-                    };
+                localStorageService.setItem(key, obj);
 
-                    var parseMarkdownContent = function () {
-                        var mdContent = '';
-                        try {
-                            mdContent = marked($scope.markdown.inputText);
-                            mdContent = '<div class="alert alert-warning" role="alert">Do not press on any link other wise your content will lost</div>' + mdContent;
-                        }
-                        catch (e) {
-                            mdContent = '<div class="alert alert-danger" role="alert">' + e + '</div>';
-                            //console.error(e.message);
-                            //throw e;
-                        }
+                $scope.cacheTime = new Date(obj.timestamp);
+            };
+            
+            var removeCache = function () {
+                var key = projectId;
+                
+                if(docId)
+                    key += '/' + docId;
 
-                        $scope.markdown.outputText = trustAsHtml(mdContent);
-                    };
+                $scope.cacheTime = undefined;
+                localStorageService.removeItem(key);
 
-                    docService.setCurrentProject(false);
+                getDocContent();
+            };
 
-                    $scope.showPreview = function () {
+            var getDocFromCache = function () {
+                var key = projectId;
+                
+                if(docId)
+                    key += '/' + docId;
 
-                        parseMarkdownContent();
+                var obj = localStorageService.getItem(key);
 
-                        $scope.isPreview = true;
-                    };
-                    
-                    $scope.openSource = function() {
-                        var base64Data = btoa($scope.markdown.inputText);
-                        $window.open('data:text/plain;base64,' + base64Data);
-                    };
-                }]);
+                if (!obj)
+                    return '';
 
-        })();
+                if((new Date()).getTime() - obj.timestamp > autoSaveExpiry) {
+                    removeCache();
+                    return '';
+                }
+
+                if (obj.content) {
+                    $scope.cacheTime = new Date(obj.timestamp);
+                }
+                
+                $scope.markdown.inputText = obj.content;
+                
+                saveToCache();
+            };
+
+            var getDocContent = function () {
+                docService.getDocContent(projectId, docId, function (data) {
+
+                    if (Object.prototype.toString.call(data) === '[object Object]') {
+                        data = data.data;
+                    }
+
+                    $scope.markdown.inputText = data;
+
+                    //parseMarkdownContent();
+                });
+            };
+
+            // auto save to local storage
+            var runTimeout = function () {
+                timer = $timeout(function () {
+                    saveToCache();
+                    runTimeout();
+                }, autoSaveDuration);
+            };
+
+            getDocFromCache();
+
+            if (!$scope.markdown.inputText) {
+                getDocContent();
+            }
+
+            var trustAsHtml = function (string) {
+                return $sce.trustAsHtml(string);
+            };
+
+            var parseMarkdownContent = function () {
+                var mdContent = '';
+                try {
+                    mdContent = marked($scope.markdown.inputText);
+                    mdContent = '<div class="alert alert-warning" role="alert">Do not press on any link other wise your content will lost</div>' + mdContent;
+                } catch (e) {
+                    mdContent = '<div class="alert alert-danger" role="alert">' + e + '</div>';
+                    //console.error(e.message);
+                    //throw e;
+                }
+
+                $scope.markdown.outputText = trustAsHtml(mdContent);
+            };
+
+            $scope.showPreview = function () {
+                parseMarkdownContent();
+                
+                $log.debug('showing preview');
+
+                if (config.editDoc.autoLocalSave) {
+                    saveToCache();
+                }
+            };
+
+            $scope.openSource = function () {
+                var base64Data = btoa($scope.markdown.inputText);
+                $window.open('data:text/plain;base64,' + base64Data);
+            };
+            
+            $scope.save = function() {
+                if(!$scope.config.enableDocSave)
+                    return;
+                
+                docService.saveDocContent(projectId, docId, $scope.markdown.inputText);
+            };
+
+            $scope.removeCache = removeCache;
+            
+            // remove timer on route change
+            $scope.$on('$destroy', function(e) {
+                $timeout.cancel(timer);
+            });
+
+            docService.setCurrentProject(false);
+
+            if (config.editDoc.autoLocalSave) {
+                runTimeout();
+            }
+        }]);
+
+})();
